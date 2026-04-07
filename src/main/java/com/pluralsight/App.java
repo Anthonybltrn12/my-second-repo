eaee3d0a15fabd1fa843a732e3072c854ca06aca
antbeltran <anthonybltrn@gmail.com>
2026-04-07 13:36:09 -0500

    second commit - add main class and print changes

--- src/main/java/com/pluralsight/App.java
+++ src/main/java/com/pluralsight/App.java
@@ -3,5 +3,6 @@ package com.pluralsight;
 public class App {
     public static void main(String[] args) {
         System.out.println("changes");
+        System.out.println("commit and push");
     }
 }
